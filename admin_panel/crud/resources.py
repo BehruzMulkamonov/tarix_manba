720ca32b28b43d5489bdad6f5200fa7ddffc4be4
temur <aliyer.temur95@gmail.com>
2024-04-05 18:53:30 +0500

    change serializer resourse

--- admin_panel/crud/resources.py
+++ admin_panel/crud/resources.py
@@ -1,51 +1,54 @@
 from rest_framework import viewsets
 
-from admin_panel.serializer.resources import CategorySerializer, PeriodFilterSerializer, FilterCategoriesSerializer, \
-    FiltersSerializer, ProvinceSerializer, ResourceSerializer, InteriveSerializer, AttributesSerializer, \
-    ContentsSerializer
+from admin_panel.serializer.resources import CategoryAdminSerializer, PeriodFilterAdminSerializer, \
+    FilterCategoriesAdminSerializer, FiltersAdminSerializer, ProvinceAdminSerializer, ResourceAdminSerializer, \
+    InteriveAdminSerializer, AttributesAdminSerializer, ContentsAdminSerializer
 from resources.models import Category, FilterCategories, Filters, Province, Resource, Interive, Attributes, Contents
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializer_class = CategoryAdminSerializer
+
+
+
 
 
 class PeriodFilterModelViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = PeriodFilterSerializer
+    serializer_class = PeriodFilterAdminSerializer
 
 
 class FilterCategoriesModelViewSet(viewsets.ModelViewSet):
     queryset = FilterCategories.objects.all()
-    serializer_class = FilterCategoriesSerializer
+    serializer_class = FilterCategoriesAdminSerializer
 
 class FiltersModelViewSet(viewsets.ModelViewSet):
     queryset = Filters.objects.all()
-    serializer_class = FiltersSerializer
+    serializer_class = FiltersAdminSerializer
 
 class ProvinceModelViewSet(viewsets.ModelViewSet):
     queryset = Province.objects.all()
-    serializer_class = ProvinceSerializer
+    serializer_class = ProvinceAdminSerializer
 
 
 class ResourceModelViewSet(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
-    serializer_class = ResourceSerializer
+    serializer_class = ResourceAdminSerializer
 
 
 class InteriveModelViewSet(viewsets.ModelViewSet):
     queryset = Interive.objects.all()
-    serializer_class = InteriveSerializer
+    serializer_class = InteriveAdminSerializer
 
 class AttributesModelViewSet(viewsets.ModelViewSet):
     queryset = Attributes.objects.all()
-    serializer_class = AttributesSerializer
+    serializer_class = AttributesAdminSerializer
 
 
 class ContentsModelViewSet(viewsets.ModelViewSet):
     queryset = Contents.objects.all()
-    serializer_class = ContentsSerializer
+    serializer_class = ContentsAdminSerializer
 
 
 
