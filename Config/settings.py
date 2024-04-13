5c913808197334d1d0e8dd1a293948efa59d40c9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-04-13 19:20:44 +0500

    posgreSQLga o'tdik

--- Config/settings.py
+++ Config/settings.py
@@ -82,10 +82,20 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tarixmanba_db',
+        'USER': 'tarixmanba_user',
+        'PASSWORD': 'tarixmanba_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
