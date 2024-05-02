f5a1751cb7d5eedc01f15c5f18be0cb1c1348973
temur <aliyer.temur95@gmail.com>
2024-05-02 14:38:02 +0500

    Library admin serializers ..

--- admin_panel/serializer/library.py
+++ admin_panel/serializer/library.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from other_app.models import Library
 
+
 class LibraryAdminSerializer(serializers.ModelSerializer):
     cat_library = serializers.SerializerMethodField()
+
     class Meta:
         model = Library
-        fields = ['id','title', 'category','author', 'type', 'year', 'country', 'language', 'image', 'file', 'created_time', 'updated_time','cat_library']
+        fields = ['id', 'title', 'category', 'author', 'type', 'year', 'country', 'language', 'image', 'file',
+                  'created_time', 'updated_time', 'cat_library']
 
-    def get_cat_library(self,obj):
+    def get_cat_library(self, obj):
         cat = obj.category
-            if cat:
-                return cat.title
+        if cat:
+            return cat.title
