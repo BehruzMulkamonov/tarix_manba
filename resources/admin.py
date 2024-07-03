d05bd64feb0a288fbfd806625db6e819a6b6291b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-07-03 18:24:11 +0500

    resursda yzgarish

--- resources/admin.py
+++ resources/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, FilterCategories, PeriodFilter, Filters, Interive
+from .models import Category, FilterCategories, PeriodFilter, Filters, Interive, Resource
 
 admin.site.register(Category)
 admin.site.register(FilterCategories)
 admin.site.register(PeriodFilter)
 admin.site.register(Filters)
 admin.site.register(Interive)
+admin.site.register(Resource)
